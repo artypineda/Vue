e3012adb7e9d9ab578fc5000fd26fd1c3d6d4630
artypineda <artypineda@gmail.com>
2020-02-25 21:30:41 -0800

    expanded

---
+++ vue_crash_todolist/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <Todos v-bind:todos="todos"/>
+  </div>
+</template>
+
+<script>
+import Todos from './components/Todos';
+
+export default {
+  name: 'App',
+  components: {
+    Todos
+  },
+  data(){
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "Todo One",
+          complete: true
+        },
+        {
+          id: 2,
+          title: "Todo Two",
+          complete: true
+        },
+        {
+          id: 3,
+          title: "Todo Three",
+          complete: false
+        },
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 4;
+}
+</style>
